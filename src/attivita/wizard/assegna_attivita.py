3e77d5b90807e4b1fed19f0a3eb4ff13d2a72dc8
norman.argiolas <norman.argiolas@flosslab.com>
2016-12-05 13:00:07 +0100

    Formatting for "attivita" module

--- src/attivita/wizard/assegna_attivita.py
+++ src/attivita/wizard/assegna_attivita.py
@@ -6,24 +6,27 @@ from openerp.osv import fields, osv
 import time
 from openerp.tools.translate import _
 
+
 class assegna_attivita(osv.osv_memory):
     _name = 'attivita.assegna.attivita'
     _description = 'Assegnazione Attivita'
 
     _columns = {
-        'assegnatario_id': fields.many2one('res.users', 'Assegnatario', required=True, domain="[('is_visible','=',True)]"),
+        'assegnatario_id': fields.many2one('res.users', 'Assegnatario',
+                                           required=True,
+                                           domain="[('is_visible','=',True)]"),
         'notifica': fields.boolean('Notifica'),
         'data_scadenza': fields.date('Data Scadenza', required=True),
-        'presa_carico_libera': fields.boolean('Assegnazione Pubblica'),       
-        
+        'presa_carico_libera': fields.boolean('Assegnazione Pubblica'),
+
     }
 
-
     def default_get(self, cr, uid, fields, context=None):
-        res = super(assegna_attivita, self).default_get(cr, uid, fields, context=context)
+        res = super(assegna_attivita, self).default_get(cr, uid, fields,
+                                                        context=context)
         if 'active_id' in context.keys():
             attivita_obj = self.pool.get('attivita.attivita')
-            attivita = attivita_obj.browse(cr, uid,context['active_id'])
+            attivita = attivita_obj.browse(cr, uid, context['active_id'])
             if attivita:
                 res['data_scadenza'] = attivita.data_scadenza
         return res
@@ -35,21 +38,29 @@ class assegna_attivita(osv.osv_memory):
         attivita_id = context and context.get('active_id') or False
         for this in self.browse(cr, uid, ids, context=context):
             context['assegnazione'] = True
-            attivita = attivita_obj.browse(cr,uid,attivita_id)
+            attivita = attivita_obj.browse(cr, uid, attivita_id)
             if attivita.state == 'assegnato' and attivita.assegnatario_id.id != uid:
-                raise osv.except_osv(_('Attenzione!'), _("Solo l'assegnatario puo' smistare l'attivita'!"))
-            attivita_obj.write(cr,uid,attivita_id,{'state': 'assegnato', 
-                                                   'data_assegnazione': time.strftime("%Y-%m-%d"),
-                                                   'data_scadenza': this.data_scadenza,
-                                                   'assegnatario_id': this.assegnatario_id.id})
+                raise osv.except_osv(_('Attenzione!'), _(
+                    "Solo l'assegnatario puo' smistare l'attivita'!"))
+            attivita_obj.write(cr, uid, attivita_id, {'state': 'assegnato',
+                                                      'data_assegnazione': time.strftime(
+                                                          "%Y-%m-%d"),
+                                                      'data_scadenza': this.data_scadenza,
+                                                      'assegnatario_id': this.assegnatario_id.id})
             # Gestione della notifica
 
-            template_model_data = self.pool.get('ir.model.data').search(cr, uid, [('name', '=', 'template_email_notifica_assegnatario_assegnazione')])
+            template_model_data = self.pool.get('ir.model.data').search(cr,
+                                                                        uid, [(
+                    'name',
+                    '=',
+                    'template_email_notifica_assegnatario_assegnazione')])
             if len(template_model_data):
                 if isinstance(ids, list):
                     ids = ids[0]
-                template_id = self.pool.get('ir.model.data').browse(cr, uid, template_model_data[0])
-                self.pool.get('email.template').generate_email(cr, uid, template_id.res_id, ids, context)
+                template_id = self.pool.get('ir.model.data').browse(cr, uid,
+                                                                    template_model_data[
+                                                                        0])
+                self.pool.get('email.template').generate_email(cr, uid,
+                                                               template_id.res_id,
+                                                               ids, context)
         return True
-    
-
--- src/attivita/wizard/wizard_attivita.py
+++ src/attivita/wizard/wizard_attivita.py
@@ -6,42 +6,54 @@ from openerp.osv import fields, osv
 import time
 from openerp.tools.translate import _
 
+
 class wizard_attivita(osv.osv_memory):
     _name = 'attivita.wizard.attivita'
     _description = 'Wizard Attivita'
 
     _columns = {
         'name': fields.text('Note', required=True),
-        'case': fields.selection([('rifiuto', 'Rifiuto'), 
+        'case': fields.selection([('rifiuto', 'Rifiuto'),
                                   ('annulla', 'Annullamento'),
-                                  ('integrazione', 'Integrazione')],'Caso', readonly=True)
+                                  ('integrazione', 'Integrazione')], 'Caso',
+                                 readonly=True)
     }
 
-
     def default_get(self, cr, uid, fields, context=None):
-        res = super(wizard_attivita, self).default_get(cr, uid, fields, context=context)
+        res = super(wizard_attivita, self).default_get(cr, uid, fields,
+                                                       context=context)
         if 'case' in context.keys():
             res['case'] = context['case']
         return res
-    
+
     def rifiuta(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
         attivita_obj = self.pool.get('attivita.attivita')
         attivita_id = context and context.get('active_id') or False
         for this in self.browse(cr, uid, ids, context=context):
-            attivita_obj.write(cr,uid,attivita_id,{'state': 'rifiutato','motivazione_rifiuto': this.name})
+            attivita_obj.write(cr, uid, attivita_id, {'state': 'rifiutato',
+                                                      'motivazione_rifiuto': this.name})
             # Gestione della notifica
             configuration_obj = self.pool.get('brains.configuration')
-            configuration_ids = self.pool.get('brains.configuration').search(cr,uid,[])
+            configuration_ids = self.pool.get('brains.configuration').search(
+                cr, uid, [])
             if len(configuration_ids) == 1:
-                configuration = configuration_obj.browse(cr,uid,configuration_ids[0])
+                configuration = configuration_obj.browse(cr, uid,
+                                                         configuration_ids[0])
             if configuration.module_attivita_notifiche and configuration.notifica_referente_rifiuto:
-                template_model_data = self.pool.get('ir.model.data').search(cr, uid, [('name', '=', 'template_email_notifica_referente_rifiuto')])
+                template_model_data = self.pool.get('ir.model.data'). \
+                    search(cr, uid, [('name', '=',
+                                      'template_email_notifica_referente_rifiuto')])
                 if len(template_model_data):
-                    template_id = self.pool.get('ir.model.data').browse(cr, uid, template_model_data[0])
-                    self.pool.get('email.template').generate_email(cr, uid, template_id.res_id,attivita_id, context)
-                    
+                    template_id = self.pool.get('ir.model.data').browse(cr,
+                                                                        uid,
+                                                                        template_model_data[
+                                                                            0])
+                    self.pool.get('email.template').generate_email(cr, uid,
+                                                                   template_id.res_id,
+                                                                   attivita_id,
+                                                                   context)
 
     def integrazione(self, cr, uid, ids, context=None):
         if context is None:
@@ -49,19 +61,25 @@ class wizard_attivita(osv.osv_memory):
         attivita_obj = self.pool.get('attivita.attivita')
         attivita_id = context and context.get('active_id') or False
         for this in self.browse(cr, uid, ids, context=context):
-            attivita_obj.write(cr,uid,attivita_id,{'state': 'lavorazione','motivazione_richiesta_integrazione': this.name, 'richiesta_integrazione': True}) 
+            attivita_obj.write(cr, uid, attivita_id, {'state': 'lavorazione',
+                                                      'motivazione_richiesta_integrazione': this.name,
+                                                      'richiesta_integrazione': True})
 
-    
     def annulla(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
         attivita_obj = self.pool.get('attivita.attivita')
         attivita_id = context and context.get('active_id') or False
         for this in self.browse(cr, uid, ids, context=context):
-            attivita_obj.write(cr,uid,attivita_id,{'state': 'annullato','motivazione_annullamento': this.name})
-            template_model_data = self.pool.get('ir.model.data').search(cr, uid, [('name', '=', 'template_email_notifica_assegnatario_annullamento')])
+            attivita_obj.write(cr, uid, attivita_id, {'state': 'annullato',
+                                                      'motivazione_annullamento': this.name})
+            template_model_data = self.pool.get('ir.model.data'). \
+                search(cr, uid, [('name', '=',
+                                  'template_email_notifica_assegnatario_annullamento')])
             if len(template_model_data):
-                template_id = self.pool.get('ir.model.data').browse(cr, uid, template_model_data[0])
-                self.pool.get('email.template').generate_email(cr, uid, template_id.res_id,attivita_id, context)
-   
-
+                template_id = self.pool.get('ir.model.data'). \
+                    browse(cr, uid, template_model_data[0])
+                self.pool.get('email.template').generate_email(cr, uid,
+                                                               template_id.res_id,
+                                                               attivita_id,
+                                                               context)

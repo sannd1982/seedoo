1ba036bdf2e66307eb1d1c621c7d4765f73605e7
daniele.sanna <daniele.sanna@flosslab.com>
2017-03-31 12:07:57 +0200

    Fixes error import attivita

--- src/attivita/__init__.py
+++ src/attivita/__init__.py
@@ -2,6 +2,5 @@
 # This file is part of Seedoo.  The COPYRIGHT file at the top level of
 # this module contains the full copyright notices and license terms.
 
-import attivita
 from . import wizard
 from . import model
